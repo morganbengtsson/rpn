6f9d9994c4f0d5283622134a1f3d99ceca63675c
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-07-14 21:16:31 +0200

    Kind of working delta time.

--- cli.rb
+++ cli.rb
@@ -1,11 +1,15 @@
 puts "Welcome to the Reverse polish notation calculator. Enter the number of 
 expressions you want to solve, followed by one expression per row:"
+
 num_expressions = gets.chomp.to_i
 expressions = []
 
 num_expressions.times do  
   expression = gets.chomp    
+  t0 = Time.now
   #result = exec('go run logger.go "' + expression + '"')
-  #puts "hej"
-  puts `go run logger.go "10 2 *"`
+  result = `go run logger.go "#{expression}"`  
+  t1 = Time.now
+  delta = t1 - t0
+  puts result.delete!("\n") + ", " + delta.to_s
 end
