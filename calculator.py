2ea2cc8987ae457ca38ccba791b423fb195f02e6
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-07-14 18:26:52 +0200

    Working evaluation

--- calculator.py
+++ calculator.py
@@ -1,17 +1,23 @@
 def calculate(operator, n0, n1):
-  return {'+' : n0 + n1, 
+  calculations = {'+' : n0 + n1, 
           '-' : n0 - n1, 
           '*' : n0 * n1, 
           '/' : n0 / n1}
+  return calculations[operator]
+
 
 def evaluate(expression):
   operators = ('+', '-', '*', '/')
   elements = expression.split()
   stack = []
   while elements:
-    element = elements.pop()
+    element = elements.pop(0)
     if element in operators:
       n0 = stack.pop()
       n1 = stack.pop()
       stack.append(calculate(element, n0, n1))
+    else:
+      stack.append(float(element))
+  return stack[0]
 
+print(evaluate("10 2 +"))
