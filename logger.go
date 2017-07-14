8b41edbf43246ba74e2d0a68025e9cb3aebc2130
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-07-14 20:43:40 +0200

    Logging to file.

--- logger.go
+++ logger.go
@@ -2,10 +2,22 @@ package main
 
 import "os/exec"
 import "fmt"
+import "os"
+import "time"
 
 func main() {
-  cmd := exec.Command("./calculator.py", "10 2 *")
+  now := time.Now()
+  expression := os.Args[1]
+  cmd := exec.Command("./calculator.py", expression)
   out, err := cmd.Output()
-  fmt.Println(string(out)) 
-  fmt.Println(err)
+
+  f, err := os.OpenFile("log.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+  if err != nil {
+    panic(err)
+  }
+  defer f.Close()
+
+  if _, err = f.WriteString(now.String() + " " + expression + "\n"); err != nil {
+    panic(err)
+  }
 }
