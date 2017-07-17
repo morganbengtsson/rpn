961b70b5ad39da6d3a284590855837be2f3d01bb
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-07-17 16:08:21 +0200

    Missing file.

---
+++ rpn.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+puts "Welcome to the Reverse polish notation calculator. Enter the number of 
+expressions you want to solve, followed by one expression per row:"
+
+num_expressions = gets.chomp.to_i
+expressions = []
+
+num_expressions.times do
+  expressions.push(gets.chomp)
+end
+
+expressions.each do |expression|
+  t0 = Time.now
+  result = `./logger "#{expression}"`
+  delta = Time.now - t0
+  puts result.delete!("\n") + ", " + delta.to_s
+end 
+
